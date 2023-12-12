10036f6d78d608909f47c2ea11333c103dc0f7b6
ShantyS24 <josesantiagosuarezperez@gmail.com>
2023-12-12 11:02:04 -0500

    cambie el index2

--- CCP/scripts/contactEvents.js
+++ CCP/scripts/contactEvents.js
@@ -56,7 +56,7 @@ export default function (contact) {
         console.debug('CDEBUG >> ContactEvents.handleContactConnected() - Contact connected to agent');
         if (contact) {
             console.debug("[contact.onConnected] Contact connected to agent. Contact state is " + contact.getStatus().type);
-            logInfoQueue("Queue Name: " +"*"+contact.getQueue().name)+"*";
+            logInfoQueue("Queue Name: " +contact.getQueue().name);
         } else {
             console.debug("[contact.onConnected] Contact connected to agent. Null contact passed to event handler");
         }
