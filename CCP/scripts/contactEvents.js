579fedcc22b82856b1292ad570a6dc40156f64f2
juanesssss <senauj91@gmail.com>
2023-12-13 15:36:49 -0500

    array bonito

--- CCP/scripts/contactEvents.js
+++ CCP/scripts/contactEvents.js
@@ -18,6 +18,7 @@ export default function (contact) {
     } else {
         console.debug("This is an existing contact for this agent");
     }
+   
     // Route to the respective handler
     contact.onIncoming(handleContactIncoming);
     contact.onAccepted(handleContactAccepted);
@@ -56,7 +57,25 @@ export default function (contact) {
     function handleContactDestroyed(contact) {
         console.debug('CDEBUG >> ContactEvents.handleContactDestroyed() - Contact will be destroyed');
         document.getElementById("QueueText").innerHTML = " ";
-    }
+            const randomMessages = [];
+            for (let i = 0; i < 5; i++) {
+                const randomIndex = Math.floor(Math.random() * messages.length);
+                randomMessages.push(messages[randomIndex]);
+            }
+    
+            Swal.fire({
+                title: "Random Messages",
+                html: randomMessages.join("<br>"),
+                showCancelButton: false,
+                confirmButtonText: "Cerrar",
+                confirmButtonColor: "green",
+            });
+            const messages = ["Message 1", "Message 2", "Message 3", "Message 4", "Message 5", "Message 6"];
+            showRandomMessages(messages);
+
+        }
+    
+
 
     function handleContactMissed(contact) {
         console.debug('CDEBUG >> ContactEvents.handleContactMissed() - Contact was missed');
